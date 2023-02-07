9d3e88c83b1ebdc7aec8cf6e77c12dfe1eacae4d
Arthur Pechenkin <freakn@Arthurs-MacBook-Air.local>
2023-02-08 01:40:20 +0700

    add menu for phones

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,36 @@
-alert("<3")
+const btn = document.getElementById("naviBtn");
+const navi = document.getElementById("navi");
+
+function windowListener() {
+    var width = window.innerWidth;
+    if (width < 767) {
+      navi.classList.add("hide");
+    } else {
+        navi.classList.remove("hide");
+    }
+}
+
+window.addEventListener('resize', function() {
+    windowListener();
+});
+
+document.getElementById("naviBtn").addEventListener("click", function() {
+    if (navi.classList.contains("hide")) {
+        navi.classList.remove("hide");
+        btn.classList.remove("menu-icon");
+        btn.classList.add("cross");
+    } else {
+        navi.classList.add("naviup");
+        btn.classList.add("menu-icon");
+        btn.classList.remove("cross");
+        setTimeout(
+            function() {
+            navi.classList.add("hide");
+            navi.classList.remove("naviup");
+            },
+            200
+        );
+    }
+});
+
+alert("<3");
