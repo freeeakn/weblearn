3cb0d5bcb31540bc7fe3cdfdd7f10681a44a7eeb
Arthur Pechenkin <freakn@Arthurs-MacBook-Air.local>
2023-02-10 22:33:49 +0700

    add exit cross for form

--- static/js/formHandler.js
+++ static/js/formHandler.js
@@ -4,6 +4,19 @@ let btnSendForm = document.querySelector("#reg-form__submit");
 let form = document.querySelector("#reg-form");
 let bgForm = document.querySelector("#form-background");
 let accBtn = document.querySelector("#account");
+let exitForm = document.querySelector("#form-exit");
+
+function ExitForm() {
+    form.classList.add('form-anime');
+    setTimeout(
+        function() {
+            form.classList.add('hide');
+            bgForm.classList.add('hide');
+            form.classList.remove('form-anime');
+        },
+        500
+    );
+}
 
 accBtn.onclick = () => {
     form.classList.remove('hide');
@@ -21,13 +34,9 @@ btnSendForm.onclick = (event) => {
 };
 
 bgForm.onclick = () => {
-    form.classList.add('form-anime');
-    setTimeout(
-        function() {
-            form.classList.add('hide');
-            bgForm.classList.add('hide');
-            form.classList.remove('form-anime');
-        },
-        500
-    );
+    ExitForm();
+};
+
+exitForm.onclick = () => {
+    ExitForm();
 };
