09cadf16113660442150557c29fb314bb055d99c
Machine Enger <freakn@Arthurs-MacBook-Air.local>
2023-02-17 23:41:35 +0700

    style js

--- static/js/gameController.js
+++ static/js/gameController.js
@@ -1,23 +1,16 @@
 function playerController(player) {
     document.addEventListener('keydown', function(event) {
-        if (event.code == 'KeyD') {
-            player.addPosX(10);
-        }
-        if (event.code == 'KeyW') {
-            player.addPosY(10);
-        }
-        if (event.code == 'KeyA') {
-            player.addPosX(-10);
-        } 
-        if (event.code == 'KeyS') {
-            player.addPosY(-10);
-        }
+        if (event.code == 'KeyD') player.addPosX(10);
+        if (event.code == 'KeyW') player.addPosY(10);
+        if (event.code == 'KeyA') player.addPosX(-10);
+        if (event.code == 'KeyS') player.addPosY(-10);
         if (event.code == 'Space') {
             player.addPosY(50);
             setTimeout(
                 function() {
                     player.addPosY(-50);
-                }, 200
+                },
+                200
             );
         }
     });
--- static/js/gameInit.js
+++ static/js/gameInit.js
@@ -2,8 +2,10 @@ const init = () => {
     
     const character = new player(50, 50);
     const coinExp = new coin(150, 150);
+
     document.getElementById('main').appendChild(coinExp.coin)
     document.getElementById('main').appendChild(character.player);
+
     playerController(character);
     setInterval(function () {
         if(character.posX == coinExp.posX) {
@@ -12,6 +14,7 @@ const init = () => {
             console.log(character.value);
         }
     }, 2000);
+    
 };
 
 init();
